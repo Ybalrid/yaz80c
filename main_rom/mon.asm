fd7d062f8d3f910ff326e8846a99e545129916ea
Arthur Brainville (Ybalrid) <ybalrid@ybalrid.info>
2019-11-19 04:00:07 +0100

    Started the shell for the monitor

--- main_rom/mon.asm
+++ main_rom/mon.asm
@@ -1,4 +1,59 @@
 org 0x1000
+
 YBAMON:
-	xor	a, a	; put 0 into A
+	call BELL
+	ld	HL, ybamon_welcome
+	call PUTS 
+
+;main loop of program
+ybamon_loop:
+	call ybamon_shell
+	jp ybamon_loop
+
+ybamon_shell:
+ybamon_shell_loop:
+	ld	HL, ybamon_prompt		;load prompt text
+	call 	PUTS				;disp text
+	call 	CLEAR_TERM_BUFFER		;clear input buffer
+ybamon_shell_buff_reset:
+	ld	HL, TERM_IBUF			;load pointer to buffer
+	ld	B, 0				;reset B to zero
+ybamon_shell_buff_watch:
+	ld	A, (HL)				;load pointed char
+	cp	0x0D				;compare with carriage return
+	jp 	Z, ybamon_shell_buff_CR		;if carraige return, jump to ouptut
+	cp	0
+	jp	Z, ybamon_shell_buff_reset
+	inc	HL				; HL++
+	inc 	B				; B++
+	push	AF				;push A
+	ld	A, (TERM_IBUF_SZ)		;load buff size
+	cp	B				;check with B
+	pop	AF				;pop A
+	jp	C, ybamon_shell_buff_reset	;if B >= TERM_IBUF_SZ, jumpto reset
+	jp 	ybamon_shell_buff_watch		;jump to buff_watch
+
+ybamon_shell_buff_CR:				;CR has been found
+						;TODO parse input
+	call	CLEAR_TERM_BUFFER			;clear buffer
+	LD 	HL, ybamon_crlf
+	call	PUTS
+	ret
+
+;DATA
+
+ybamon_welcome:
+db "Starting YbaMon, Ybalrid's z80 monitor\r\n",0
+
+ybamon_prompt:
+db "? ",0
+
+ybamon_crlf:
+db 0x0D, 0x0A, 0
+
+ybamon_ok:
+db "OK\r\n",0
+
+ybamon_synerr:
+db "SYN ERR\r\n", 0
 
--- main_rom/rom.asm
+++ main_rom/rom.asm
@@ -1,40 +1,210 @@
 ; rom.asm
 ;
-; From the yaz80c project
+; From the yaz80c project - yet another z80 computer
 ;
-; This file is the main file tha build the rom
+; This file is the main file to build the main 32K system rom
 
 
 ;; Address table configuration
 ; DART channel A
 DART_A_C:	equ	0x0000
 DART_A_D:	equ	0x0001
+DART_B_C:	equ	0x0010
+DART_B_D:	equ	0x0011
 
 ;; RAM configuraiton
-TERM_IBUF:	equ	0x8000	; serial input buffer
-TERM_IBUF_SZ:	equ	0x80FF	; bytes used in serial input buffer
-TERM_IBUF_TMP:	equ	0x80FE
 RAM_START:	equ	0x8100	; start of general purpose ram
 STACK_START:	equ	0xFFFF	; inital value of stack pointer
 
 ;; Serial input buffer
-;TODO
+TERM_IBUF:	equ	0x8000	; serial input buffer
+TERM_IBUF_SZ:	equ	0x80FF	; bytes used in serial input buffer
+TERM_IBUF_TMP:	equ	0x80FE
 
 org 0x0000
-RESET:				; code to be placed at reset vector
+CPU_RESET:					; code to be placed at reset vector
 	di				; disable all interupts until computer has finished boot
 	ld	SP, (STACK_START)	; put stack pointer at the very top of RAM
-	call	BOOTUP
+	call	BOOTUP			; run hardware boot program
+	jp YBAMON			; Jumpt to one program installed in ROM: Ybalrd's monitor
 
-;define interupt vectors
+
+; Define interupt vectors table - this system runs on Interupt Mode 2.
+; We keep register I at 0x00
 defs 0x0C-$
-INT_VECT:
-RX_CHA_AVAIL:				; 0x000C = character available from terminal
+defw	RX_CHA_AVAIL			; 0x000C = character available from terminal
+defs 0x0E-$
+defw SPEC_RX_COND
+
+
+SPEC_RX_COND:
+	jp CPU_RESET
+	
+defs 0x66-$
+NMI:
+	nop
+	ei
+	retn	
+
+; DART configuration
+INIT_DART:
+	ld	A, 0
+	ld	I, A
+	ld	(TERM_IBUF_SZ), A
+
+	;TODO write configureation bytes in DART_A_C
+	
+	ld	A, %00110000	;WR0 error reset, select WR0
+	out	(DART_A_C), A
+	ld	A, 0x18		;WR0 channel reset
+	out	(DART_A_C), A
+	ld 	A, 0x4		;Select WR4
+	out	(DART_A_C), A
+	ld 	A, 0x44		;Set clock 16, 1 stop bit no parity
+	out	(DART_A_C), A
+	ld	A, 0x5		;Select WR5
+	out	(DART_A_C), A
+	ld	A, 0xE8		;DTR active, TX 8bit, Break Off, TX on RTS not active
+	out	(DART_A_C), A
+	ld	A, 0x1		;Select WR1 (B)
+	out	(DART_B_C), A
+	ld	A, %00000100
+	out	(DART_B_C), A	;CHB interupt diabled, special RX affect vect
+
+	ld	A, 0x2		;Select WR2 (B)
+	out	(DART_B_C), A
+	ld	A, 0x0		;Set interupt vector, D3, D2, D1 depend on RX
+	out	(DART_B_C), A
+
+	ld	A, 0x1		;Select WR0
+	out	(DART_A_C), A
+	ld 	A, %00011000	;Interupt all character RX
+				;parity interupt OFF
+				;buffer overrun interupt ON 
+	out	(DART_A_C), A
+
+;TODO push pop registers
+DART_A_EI: ;enable RX interupts
+	ld	A, 0x3
+	out	(DART_A_C), A
+	ld	A, 0xC1
+	out	(DART_A_C), A
+	ret
+
+DART_A_DI: ;disable RX interupts
+	ld	A, 0x3
+	out	(DART_A_C), A
+	ld	A, 0xC0
+	out	(DART_A_C), A
+	ret
+
+A_RTS_OFF:
+	ld	A, 0x5
+	out 	(DART_A_C), A
+	ld	A, 0xE8
+	out	(DART_A_C), A
+	ret
+
+A_RTS_ON:
+	ld	A, 0x5
+	out 	(DART_A_C), A
+	ld	A, 0xEA
+	out	(DART_A_C), A
+	ret
+
+
+A_TX_EMP:
+	sub	A
+	inc	A
+	out	(DART_A_C), A
+	in	A, (DART_A_C)
+	bit 	0, A
+	jp 	Z,A_TX_EMP
+	ret
+	
+CLEAR_TERM_BUFFER:
+	LD	A, (TERM_IBUF_SZ)
+	LD	E, A
+	LD	HL, TERM_IBUF
+CLEAR_TERM_LOOP:
+	LD	A, 0
+	LD	(HL), A
+	INC	HL
+	LD	A, L
+	CP	E
+	JP	C, CLEAR_TERM_LOOP
+	LD 	A, 0
+	LD	(TERM_IBUF_SZ), A
+	ret
+
+BOOTUP:
+	call INIT_DART
+	ld	HL, BOOT_MSG_STR 
+	call	PUTS
+	call CLEAR_TERM_BUFFER		; clear terminal input buffer (for e.g: soft reset)
+	im 2
+	ei
+	call	A_RTS_ON	
+	ret
+
+;print the character in A
+PUTCHAR:
+	out (DART_A_D), A
+	call A_TX_EMP
+	ret
+
+BELL:
+	LD	A,0x07
+	call PUTCHAR
+	ret
+
+;print the string pointed by HL
+PUTS:
+	push	HL
+PUTS_LOOP:
+	ld 	A, (HL)
+	cp 	0
+	jp	z, PUTS_END
+	call PUTCHAR
+	inc HL
+	jp PUTS_LOOP
+PUTS_END:
+	POP	HL
+	ret
+
+;print 8bit hex number in A
+PUT_HEX:
+;TODO display hex number on terminal
+	ret	
+
+;print 16bit number in HL
+PUT_HEX16:
+;TODO display hex number on terminal
+	LD A, H
+	call PUT_HEX
+	LD A, L
+	call PUT_HEX
+	ret
+
+
+RX_CHA_AVAIL:				
 	push	AF
 	push 	HL
 	push 	DE
 	in	A, (DART_A_D)		; READ one character from DART A channel
 	ld	(TERM_IBUF_TMP), A	; Store character in temp storage
+	
+	cp	0x0D
+	jp	Z, RX_CHA_EXPLICT_ACCEPT
+	cp 	0x08			;check if backspace
+	jp	Z, RX_CHA_BACKSPACE
+	cp	0x20			;ignore non printabel char here
+	jp	C, RX_CHA_NO_PRINT
+	cp	0x7E
+	jp	NC, RX_CHA_NO_PRINT
+RX_CHA_EXPLICT_ACCEPT:
+	out	(DART_A_D), A		; Assume no echo local on terminal
+	call	A_TX_EMP		; wait
 	
 	; At this point, the character is stored from the device to RAM
 	; We need to check the size of the input buffer
@@ -58,44 +228,48 @@ RX_CHA_AVAIL:				; 0x000C = character available from terminal
 	jp	RX_CHA_AVAIL_END
 
 RX_CHA_BUFF_FULL:
-	nop				; todo signal that buffer is full
+			; todo signal that buffer is full
 					; this is an error condition we need to recuperate from
+RX_CHA_BACKSPACE:
+	ld	A, (TERM_IBUF_SZ)
+	cp	0
+	JP	NZ, RX_CHA_BACKSPACE_NO_EMPTY
+	JP 	RX_CHA_NO_PRINT
+RX_CHA_BACKSPACE_NO_EMPTY:
+	dec	A
+	cp	255
+	JP	NZ, RX_CHA_BACKPACE_NO_OVERFLOW
+	xor	A
+RX_CHA_BACKPACE_NO_OVERFLOW:
+	ld	(TERM_IBUF_SZ), A
+	ld	HL, TERM_IBUF
+	ld	D, 0
+	ld	E, A
+	add	HL, DE
+	ld	A, 0
+	ld	(HL), A
+
+	;Now, erase last character on the terminal by printing erase sequece
+	ld	HL, ERASE_SEC
+	call	PUTS
+	jp 	RX_CHA_AVAIL_END		
+RX_CHA_NO_PRINT:
+	call BELL
 RX_CHA_AVAIL_END:
 	pop	DE
 	pop	HL
 	pop	AF
 	ei
 	reti
-	
-defs 0x66-$
-NMI:
-	nop
-	ei
-	retn	
 
-; DART configuration
-INIT_DART:
-	ld	A, 0
-	ld	(TERM_IBUF_SZ), A
-	ld	I, A
 
-	;TODO write configureation bytes in DART_A_C
+BOOT_MSG_STR:
+db "yaz80c 8bit z80 computer\r\n(C) 2019 Arthur Brainville (Ybalrid)\r\nThis is open-source hardware and software\r\n",0
 
-	im	2		; config interupt mode 2
-	ei			; enable maskable interupt
-	ret
-
-CLEAR_TERM_BUFFER:
-	push 	AF
-	xor 	A
-	ld	(TERM_IBUF_SZ), A
-	pop	AF
-	ret
-
-BOOTUP:
-	call INIT_DART
-	ret
+ERASE_SEC:
+db 0x08, 0x20, 0x08, 0
 
 defs 0x1000-$
 include "mon.asm"
 
+defs 0x8000-$
